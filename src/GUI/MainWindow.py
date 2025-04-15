5daa03dfcb7de96ee91cd5a9a6f35563c9b71c16
kpogodzinski <kacperpogodzinski@spoko.pl>
2025-04-15 12:55:30 +0200

    Plik z przykładowymi danymi oraz wyświetlanie wpisów w oknie głównym.

--- src/GUI/MainWindow.py
+++ src/GUI/MainWindow.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import pandas as pd
 from tkinter import ttk
 from GUI.NewExpense import NewExpense
 
@@ -14,13 +15,21 @@ class MainWindow(tk.Tk):
         self.resizable(width=False, height=False)
 
         ttk.Label(self, text="Lista wydatków", font="24").pack()
-        cols = ("Data", "Kwota", "Kategoria", "Uwagi")
+
+        file = pd.read_csv("expenses.csv", sep=";", date_format="%yyyy-%MM-%dd")
+        expenses_list = ttk.Treeview(self)
+        expenses_list["columns"] = list(file.columns)
+        expenses_list["show"] = "headings"
         widths = (100, 100, 150, 250)
-        expenses_list = ttk.Treeview(self, columns=cols, show="headings")
-        for col, width in zip(cols, widths):
+        for col, width in zip(file.columns, widths):
             expenses_list.heading(col, text=col)
             expenses_list.column(col, width=width)
+
+        for _, row in file.iterrows():
+            expenses_list.insert("", tk.END, values=list(row))
+
         expenses_list.pack()
+
 
         new_exp_btn = ttk.Button(self, text="Nowy wydatek", command=new_expense)
         new_exp_btn.pack()
